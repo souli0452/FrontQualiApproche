c3279cd85ab7dfaac433484a668b380e9c86bf7c
MACBOOK   PRO <macbook@192.168.11.106>
2026-05-31 09:54:30 +0000

    refact search

--- src/app/models.ts
+++ src/app/models.ts
@@ -308,6 +308,7 @@ export interface KcUser {
     emailVerified: boolean;
     firstName: string;
     lastName: string;
+    fonction?: string;
     email: string;
     password: string;
     structure: string;
