2a4acde639a5f53f192e7bfe5061fc5d3ba6aeae
David-Joe-dev <nayokouassi@hotmail.com>
2026-08-12 07:56:07 +0100

    David: traduction en français et style QualiSira de la page access

--- src/app/pages/auth/access.ts
+++ src/app/pages/auth/access.ts
@@ -11,17 +11,17 @@ import { RippleModule } from 'primeng/ripple';
     template: `
         <div class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden">
             <div class="flex flex-col items-center justify-center">
-                <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, rgba(247, 149, 48, 0.4) 10%, rgba(247, 149, 48, 0) 30%)">
+                <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, color-mix(in srgb, var(--primary-color), transparent 60%) 10%, transparent 30%)">
                     <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20 flex flex-col items-center" style="border-radius: 53px">
                         <div class="gap-4 flex flex-col items-center">
-                            <div class="flex justify-center items-center border-2 border-orange-500 rounded-full" style="width: 3.2rem; height: 3.2rem">
-                                <i class="text-orange-500 pi pi-fw pi-lock !text-2xl"></i>
+                            <div class="flex justify-center items-center  rounded-full" style="width: 6rem; height: 6rem">
+                                <img src="assets/logo-quali-sira.svg" alt="QualiSira" class="w-30 h-30 shrink-0" />
                             </div>
-                            <h1 class="text-surface-900 dark:text-surface-0 font-bold text-4xl lg:text-5xl mb-2">Access Denied</h1>
-                            <span class="text-muted-color mb-8">You do not have the necessary permisions. Please contact admins.</span>
-                            <img src="https://primefaces.org/cdn/templates/sakai/auth/asset-access.svg" alt="Access denied" class="mb-8" width="80%" />
-                            <div class="col-span-12 mt-8 text-center">
-                                <p-button label="Go to Dashboard" routerLink="/" severity="warn" />
+                            <h1 class="text-surface-900 dark:text-surface-0 font-bold text-4xl lg:text-5xl mb-2 text-center">Accès refusé</h1>
+                            <span class="text-muted-color mb-8 text-center">Vous ne disposez pas des autorisations nécessaires. Veuillez contacter les administrateurs.</span>
+                            <img src="https://primefaces.org/cdn/templates/sakai/auth/asset-access.svg" alt="Accès refusé" class="mb-2 grayscale" width="80%" />
+                            <div class="col-span-12 mt-4 text-center">
+                                <p-button label="Retourner au tableau de bord" routerLink="/" icon="pi pi-home" severity="info" />
                             </div>
                         </div>
                     </div>
